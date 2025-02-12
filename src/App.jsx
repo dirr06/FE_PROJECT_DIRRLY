f9223d0f1fab12c7d6fa53f5f927ff9b2687451f
Jeffrryy <nickochristian642@gmail.com>
2025-02-12 21:10:32 +0700

    semuanya udah jadi baru anjai

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,18 +8,22 @@ import Navbar from './component/Navbar.jsx'
 import Booking from './page/Booking.jsx'
 import Tickets from './page/Tickets.jsx'
 import Flights from './page/Flights.jsx'
-function App() {
-  const [count, setCount] = useState(0)
+import Login from './page/Login'
+import Register from './page/Register'
+import ProtectedRoute from './component/ProtectedRoute.jsx'
 
+const App = () => {
   return (
     <div>
     <Router>
     <Navbar/>
     <Routes>
-      <Route path="/FE_PROJECT_DIRRLY" element={<Home/>}/>
-      <Route path="/FE_PROJECT_DIRRLY/booking" element={<Booking/>}/>
-      <Route path="/FE_PROJECT_DIRRLY/tickets" element={<Tickets/>}/>
-      <Route path="/FE_PROJECT_DIRRLY/flights" element={<Flights/>}/>
+      <Route path="/FE_PROJECT_DIRRLY/login" element={<Login/>}/>
+      <Route path="/FE_PROJECT_DIRRLY/register" element={<Register/>}/>
+      <Route path="/FE_PROJECT_DIRRLY" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+      <Route path="/FE_PROJECT_DIRRLY/booking" element={<ProtectedRoute><Booking/></ProtectedRoute>}/>
+      <Route path="/FE_PROJECT_DIRRLY/tickets" element={<ProtectedRoute><Tickets/></ProtectedRoute>}/>
+      <Route path="/FE_PROJECT_DIRRLY/flights" element={<ProtectedRoute><Flights/></ProtectedRoute>}/>
     </Routes>
     </Router>
     </div>
